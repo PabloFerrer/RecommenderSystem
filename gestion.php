3cdbc3c6a3f39a0d8f8f9da97f498ca94c9baef5
JavierRelinque <javierrelinque@hotmail.com>
2019-05-23 18:54:35 +0200

    more ratings

--- gestion.php
+++ gestion.php
@@ -33,10 +33,15 @@
 			
 			<h4>Valoración:</h4>
 			<select name="score" id="score">
+				<option value="0.5">0.5</option>
 				<option value="1">1</option>
+				<option value="1.5">1.5</option>
 				<option value="2">2</option>
+				<option value="2.5">2.5</option>
 				<option value="3">3</option>
+				<option value="3.5">3.5</option>
 				<option value="4">4</option>
+				<option value="4.5">4.5</option>
 				<option value="5">5</option>
 			</select>
 			<input type="button" id="ratemovie" value="Votar"></input>
